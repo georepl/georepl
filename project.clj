228f0ff3cf600d86801519f0bc2dba8291fe1dcd
georepl <georepl@users.noreply.github.com>
2016-01-22 19:42:35 +0100

    Create project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject georepl "0.1.0-SNAPSHOT"
+  :description "Mathematics with Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [thi.ng/math "0.1.4"]
+                 [quil "2.2.6"]
+                 [lein-light-nrepl "0.2.0"]])
+                 
